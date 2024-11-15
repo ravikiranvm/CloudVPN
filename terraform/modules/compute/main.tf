a3828ccbcc79c6db8521a0b7efc378461ce14c6d
Ravi Kiran Vallamkonda <ravikiran@Ravis-MacBook-Air.local>
2024-11-15 21:11:53 +0530

    Basic Infra setup (cleared bug - terraform wanted to replace the ec2 for every terraform apply without any changes

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -7,8 +7,9 @@ resource "aws_instance" "wireguard_instance" {
     ami = "ami-07c9c7aaab42cba5a"
     instance_type = "t2.micro"
     subnet_id = var.subnet_id
-    security_groups = [var.security_group_id] 
-
+    
+    #security_groups = [var.security_group_id] # security_groups is for classic ec2 and default vpc only
+    vpc_security_group_ids = [var.security_group_id]
 
     associate_public_ip_address = true
 
